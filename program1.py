7124410c46ab20978d4be22cce75f3be7b1fd0d3
alokkumar45 <alokku981870@gmail.com>
2024-09-09 17:56:29 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,7 @@
+#  Write a program to calculate area of rectangle 
+
+length = int(input("Enter the length of  rectangle :"))
+width = int(input("Enter the width of rectangle :"))
+area = (length*width)
+print("calculate of rectangle :",area)
+
